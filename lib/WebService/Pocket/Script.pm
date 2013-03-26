2312aaeeddcf180faf553019fc249fe46b60f834
Andreas Marienborg <andreas.marienborg@gmail.com>
2013-03-26 17:07:38 +0700

    Add documentation to command line script
    This takes the form mainly of a small blurb in the Script.pm, and hiding attributes from --help.
    
    Also satisfy some dzil warnings

--- lib/WebService/Pocket/Script.pm
+++ lib/WebService/Pocket/Script.pm
@@ -1,5 +1,5 @@
 package WebService::Pocket::Script;
-
+#ABSTRACT: Wrap up WebService::Pocket into a runable script with a config file
 use Moose;
 
 extends 'WebService::Pocket'; # XXX: this would ideally be a role :/
@@ -8,7 +8,27 @@ with 'MooseX::SimpleConfig';
 with 'MooseX::Getopt';
 
 # XXX: is this good enough?
-has '+configfile' => ( default => $ENV{HOME} . "/.pocketrc" );
+has '+configfile' => ( default => $ENV{HOME} . "/.pocketrc", traits => [qw/NoGetopt/] );
+has "+$_" => ( traits => [qw/NoGetopt/] ) for qw/api_key base_url ua json items list_since/;
+has "+$_" => (documentation => "Set $_. Can also be set in ~/.pocketrc")
+for qw/username password/;
+
+before 'print_usage_text' => sub {
+    print <<END;
+$0 - Wrap up Webservice::Pocket in a script
+
+Configure in ~/.pocketrc, add something like:
+
+username=myusername
+password=s3cr3t
+
+Commands:
+
+  add <url> [<url>]: Add url(s) to your pocket list
+
+END
+};
+
 
 sub config_any_args {
     return {
@@ -18,7 +38,6 @@ sub config_any_args {
 
 sub run {
     my $self = shift;
-    require Data::Dump;
     my @args = @{ $self->extra_argv };
     my $cmd = shift @args;
     unless ($cmd) {
