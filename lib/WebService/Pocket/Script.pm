ec9b51df4e3f9e85af0d417039f1de0f4a09188e
Andreas Marienborg <andreas.marienborg@gmail.com>
2013-03-27 08:59:04 +0700

    Code cleanup after review

--- lib/WebService/Pocket/Script.pm
+++ lib/WebService/Pocket/Script.pm
@@ -8,8 +8,14 @@ with 'MooseX::SimpleConfig';
 with 'MooseX::Getopt';
 
 # XXX: is this good enough?
-has '+configfile' => ( default => $ENV{HOME} . "/.pocketrc", traits => [qw/NoGetopt/] );
-has "+$_" => ( traits => [qw/NoGetopt/] ) for qw/api_key base_url ua json items list_since/;
+has '+configfile' => (
+    default => $ENV{HOME} . "/.pocketrc",
+    traits => [qw/NoGetopt/]
+);
+
+has "+$_" => ( traits => [qw/NoGetopt/] )
+for qw/api_key base_url ua json items list_since/;
+
 has "+$_" => (documentation => "Set $_. Can also be set in ~/.pocketrc")
 for qw/username password/;
 
@@ -31,13 +37,14 @@ END
 
 
 sub config_any_args {
+    my ($self) = @_;
     return {
         force_plugins => ['Config::Any::INI'],
     }
 }
 
 sub run {
-    my $self = shift;
+    my ($self) = @_;
     my @args = @{ $self->extra_argv };
     my $cmd = shift @args;
     unless ($cmd) {
@@ -49,13 +56,13 @@ sub run {
 }
 
 sub cmd_add {
-    my $self = shift;
+    my ($self, @args) = @_;
 
     my @items = map {
     {
         url => $_
     }
-    } @_;
+    } @args;
     my $res = $self->add(\@items);
     print join("\n*", map { $_->url } @$res);
 }
